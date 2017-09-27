8ec4e7ce73ed4d4016f882671c73ea0e5d85ab57
heron <heron.rossi@hotmail.com>
2017-09-27 08:41:15 -0300

    Playing

--- src/ClientTypes.hs
+++ src/ClientTypes.hs
@@ -9,9 +9,29 @@ data Person = Person String String Gender
 data Gender = Male | Female | Unknown
             deriving Show
 
+data GenderCount = GenderCount Int Int Int
+            deriving Show
+
 clientName:: Client -> String
 clientName client = case client of
                     GovOrg name -> name
                     Company name id person addrs -> name
                     Individual person ->
                         case person of Person fName lName gender -> fName ++ " " ++ lName
+
+companyName:: Client -> Maybe String
+companyName company = case company of
+                      Company name _ _ _ -> Just name
+                      _                  -> Nothing
+
+personGender:: Person -> Int
+personGender (Person _ _ gender) = case gender of
+                                   Male    -> 1
+                                   Female  -> 0
+                                   Unknown -> 0
+
+countGender:: [Person] -> Int
+countGender personList = let p = (head personList)
+                         in if null (tail personList) then
+                             personGender p
+                            else personGender p + countGender (tail personList)
